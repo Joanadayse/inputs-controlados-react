d963ddb15c330a46f412fdbc33ca374bdf8da7e8
Joanadayse <daysejoana3@gmail.com>
2024-10-01 07:48:08 -0300

    primeiro commit

---
+++ src/Components/MainPage/MainPage.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import { MainContainer, Form, Input } from './styles'
+
+function MainPage() {
+  return (
+    <MainContainer>
+      <h2>Formulário de inscrição</h2>
+    
+    </MainContainer>
+  )
+}
+
+export default MainPage
